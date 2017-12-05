021f8f023984ee41c9d39a3237119b35ec5f886f
Matt <mboddewy@u.rochester.edu>
2017-12-04 23:24:53 -0500

    Add Circuit(a) as well as Circuit(a) tester

--- main.c
+++ main.c
@@ -50,20 +50,6 @@ static Circuit* Circuits_or3() {
 	return new_Circuit(3, inputs, 1, outputs, 2, gates);
 }
 
-//Matt method to test INVERTER to make a 1 input NOT circuit
-static Circuit* Circuit_not1() {
-	Value* in0 = new_Value(false);
-	Gate* not0 = new_Inverter(in0);
-
-	Value** inputs = new_Value_array(1);
-	inputs[0] = in0;
-	Value** outputs = new_Value_array(1);
-	outputs[0] = Gate_getOutput(not0);
-	Gate** gate = new_Gate_array(1);
-	gate[0] = not0;
-	return new_Circuit(1, inputs, 1, outputs, 1, gate);
-}
-
 static char* b2s(bool b) {
 	return b ? "T" : "F";
 }
@@ -78,11 +64,25 @@ static void test3In1Out(Circuit* circuit, bool in0, bool in1, bool in2) {
 	printf("%s %s %s -> %s\n", b2s(in0), b2s(in1), b2s(in2), b2s(out0));
 }
 
+//Matt method to test INVERTER to make a 1 input NOT circuit
+static Circuit* Circuit_not1() {
+	Value* in0 = new_Value(false);
+	Gate* not0 = new_Inverter(in0);
+
+	Value** inputs = new_Value_array(1);
+	inputs[0] = in0;
+	Value** outputs = new_Value_array(1);
+	outputs[0] = Gate_getOutput(not0);
+	Gate** gate = new_Gate_array(1);
+	gate[0] = not0;
+	return new_Circuit(1, inputs, 1, outputs, 1, gate);
+}
+
 //Matt method to test NOT gate
-static void test1In1Out(Circuit* circuit, bool in0) {
-	Circuit_setInput(circuit, 0, in0);
-	Circuit_update(circuit);
-	bool out0 = Circuit_getOutput(circuit, 0);
+static void test1In1Out(Circuit* notCircuit, bool in0) {
+	Circuit_setInput(notCircuit, 0, in0);
+	Circuit_update(notCircuit);
+	bool out0 = Circuit_getOutput(notCircuit, 0);
 	printf("%s -> %s\n", b2s(in0), b2s(out0));
 }
 
@@ -91,41 +91,32 @@ static Circuit* Circuits_ca() {
 	Value* x = new_Value(false);
 	Value* y = new_Value(false);
 	Value* z = new_Value(false);
-	Gate* notY = new_Inverter(y);
+	Gate* noty = new_Inverter(y);
+	Gate* xAndNoty = new_AndGate(Gate_getOutput(noty), x);
+	Gate* yAndz = new_AndGate(y, z);
+	Gate* finalOR = new_OrGate(Gate_getOutput(xAndNoty), Gate_getOutput(yAndz));
 
 	Value** inputs = new_Value_array(3);
 	inputs[0] = x;
 	inputs[1] = y;
 	inputs[2] = z;
-	Value** outputs = new_Value_array(4);
-	outputs[0] = Gate_getOutput(notY); //Storing value of !y
-	Value_print(outputs[0]);
-
-	Gate* xAndNoty = new_AndGate(x, outputs[0]); //AND gate for x AND !y
-	outputs[1] = Gate_getOutput(xAndNoty); //Storing value for x AND !y
-
-	Gate* yAndz = new_AndGate(y, z); //AND gate of y AND z
-	outputs[2] = Gate_getOutput(yAndz); //Storing value for y AND z
-
-	Gate* finalOr = new_OrGate(outputs[1], outputs[2]); //OR gate for (x!y + yz)
-	outputs[3] = Gate_getOutput(finalOr);
-
+	Value** outputs = new_Value_array(1);
+	outputs[0] = Gate_getOutput(finalOR);
 	Gate** gates = new_Gate_array(4);
-	gates[0] = notY;
+	gates[0] = noty;
 	gates[1] = xAndNoty;
 	gates[2] = yAndz;
-	gates[3] = finalOr;
-	return new_Circuit(3, inputs, 1, outputs, 2, gates);
+	gates[3] = finalOR;
+	return new_Circuit(3, inputs, 1, outputs, 4, gates);
 }
 
-static void testCircuita(Circuit* circuit, bool x, bool y, bool z) {
-	Circuit_setInput(circuit, 0, x);
-	Circuit_setInput(circuit, 1, y);
-	Circuit_setInput(circuit, 2, z);
-	//Circuit_dump(circuit);
-	Circuit_update(circuit);
-	bool out3 = Circuit_getOutput(circuit, 3);
-	printf("%s %s %s -> %s\n", b2s(x), b2s(y), b2s(z), b2s(out3));
+static void testCircuita(Circuit* circuitA, bool x, bool y, bool z) {
+	Circuit_setInput(circuitA, 0, y);
+	Circuit_setInput(circuitA, 1, x);
+	Circuit_setInput(circuitA, 2, z);
+	Circuit_update(circuitA);
+	bool out0 = Circuit_getOutput(circuitA, 0);
+	printf("((!%s)%s)+((%s)%s) -> %s\n", b2s(y), b2s(x), b2s(y), b2s(z), b2s(out0));
 }
 
 int main(int argc, char **argv) {
@@ -151,8 +142,8 @@ int main(int argc, char **argv) {
 	Circuit* cNOT0 = Circuit_not1();
 	test1In1Out(cNOT0, true);
 
-	printf("Circuit(a) all inputs true: should be true\n");
+	printf("Circuit(a) inputs true: should be true\n");
 	Circuit* cA = Circuits_ca();
-	testCircuita(cA, true, true, true);
+	testCircuita(cA, false, true, true);
 	//End Matt test cases
 }
